05b2f520548f0fe0180e2b967364f72ddb62cf54
yiding he <yidinghe2012@gmail.com>
2017-04-28 22:26:50 -0400

    Init commit for code
    Code commit

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+	public float bunnyJumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("Jump")) {
+			myRigidbody.AddForce (transform.up*bunnyJumpForce);
+		}
+	}
+}
